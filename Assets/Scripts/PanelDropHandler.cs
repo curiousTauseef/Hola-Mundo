9abb0248e51740b137b35697639dc330178a1327
Itachi-Ichigo <siddartha1191@gmail.com>
2016-02-23 18:18:20 -0700

    Initial commit for dialogue system

---
+++ Assets/Scripts/PanelDropHandler.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class PanelDropHandler : MonoBehaviour, IDropHandler {
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnDrop (PointerEventData eventData)
+	{
+		Debug.Log ("panel drop");
+
+		GameObject go = eventData.pointerDrag;
+
+		if (go.transform.parent != this.transform) {
+					go.transform.parent = this.transform;
+		}
+
+	}
+
+
+
+}
